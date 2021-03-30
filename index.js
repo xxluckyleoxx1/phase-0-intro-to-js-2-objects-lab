04366c34d6272463da0a4c203287d7f7528ba62f
github username <github email address>
2021-03-30 01:49:57 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: 'jeff',
+    streetAddress: '34 Ackerman St. Daywood, NY 10964'
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmp = {...employee};
+    newEmp[key] = value;
+    return newEmp;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmp = {...employee};
+    delete newEmp[key];
+    return newEmp;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
